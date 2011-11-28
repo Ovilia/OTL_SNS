88d5e9b421d2e98c9035fdb7706c53eb69ed1ec2
Ovilia <zwl.sjtu@gmail.com>
2011-11-28 10:58:03 +0800

    Add search column

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -26,27 +26,36 @@
         <div id="heading-content">
             <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png" style="float: left; margin-right: 20px; " />
             <div class="heading-nav">
-                <a href="../" class="selected">Home</a>
+                <a href="#" class="selected">首页</a>
             </div>
             <span class="heading-span">|</span>
             <div class="heading-nav">
-                <a href="#">Profile</a>
+                <a href="#">好友</a>
             </div>
             <span class="heading-span">|</span>
             <div class="heading-nav">
-                <a href="#">Friend Feeds</a>
+                <a href="#">私信</a>
             </div>
             <span class="heading-span">|</span>
             <div class="heading-nav">
-                <a href="#">Message</a>
+                <a href="#">课程</a>
             </div>
             <span class="heading-span">|</span>
             <div class="heading-nav">
-                <a href="#">Logout</a>
+                <a href="../">登出</a>
             </div>
+
             <div class="heading-ava">
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ava.jpeg" />
             </div>
+
+            <div class="search">
+                <form action="" method="post" name="search" id="search">
+                    <input name="keyword" type="text" class="input" id="keyword" value="" style="float:left;">
+                    <label for="keyword" id="for-keyword" class="label">我要搜索</label>
+                    <span class="submit" onclick="return formSubmit('frmsearch');">搜索</span>
+                </form>
+            </div><!-- End of search -->
         </div><!-- End of heading-content -->
     </div><!-- End of floatHeader -->
 
@@ -61,5 +70,31 @@
 
 </div><!-- page -->
 
+    <!-- For search menu -->
+    <script type="text/javascript">
+        var $ = function(id){return document.getElementById(id);}
+        var formSubmit = function(id){
+            document.forms[id].submit();
+            return false;
+        }
+        var tip = function(q, for_q){
+            q = $(q);
+            for_q = $(for_q);
+            q.onfocus = function(){
+                for_q.style.display = 'none';
+                q.style.backgroundPosition = "right -17px";
+            }
+            q.onblur = function(){
+                if(!this.value) for_q.style.display = 'block';
+                q.style.backgroundPosition = "right 0";
+            }
+            for_q.onclick = function(){
+                this.style.display = 'none';
+                q.focus();
+            }
+        };
+        tip('keyword','for-keyword');
+    </script>
+    
 </body>
 </html>
