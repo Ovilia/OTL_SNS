b824d34eafe0bf2e6844601a085bfac3035665b9
Ovilia <zwl.sjtu@gmail.com>
2011-12-01 14:05:31 +0800

    Change layout of home page

---
+++ protected/views/layouts/home.php
@@ -0,0 +1,15 @@
+<?php $this->beginContent('//layouts/main'); ?>
+<div class="container">
+	<div class="span-15">
+		<div id="content">
+			<?php echo $content; ?>
+		</div><!-- content -->
+	</div>
+	<div class="span-9 last">
+		<div id="sidebar">
+		<?php
+		?>
+		</div><!-- sidebar -->
+	</div>
+</div>
+<?php $this->endContent(); ?>
