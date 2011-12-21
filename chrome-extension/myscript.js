9d9bec18a61f9946b791da41c2c6a16243291c78
tecton <tecton69@gmail.com>
2011-12-21 12:24:18 +0800

    "Import classes chrome extension."

---
+++ chrome-extension/myscript.js
@@ -0,0 +1,16 @@
+function click() {
+	        //chrome.tabs.executeScript(null,{code:"document.body.style.backgroundColor='" + color.id + "'"});
+	        //window.close();
+	        //chrome.tabs.executeScript(null,{code:"alert('123');"});
+	        /*var html = $.ajax({ url: "http://www.baidu.com", async: false }).responseText;
+			        $.ajax({
+						            type: "POST",
+						            url: "localhost/test.php",
+						            data: ("text=" + html),
+						            success: function{chrome.tabs.executeScript(null,{code:"alert('ok');"});},
+						            error: function{}
+					        });
+					        //chrome.tabs.executeScript(null,{file:"getClass.js"});
+			*/
+	alert('123');
+}
